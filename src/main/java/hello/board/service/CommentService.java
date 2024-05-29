bddf7552b95d672b3966138ea2c2fc9be96a2248
geunhokinn <gghh1220@gmail.com>
2024-05-29 23:27:32 +0900

    feat: 댓글 수정 서비스 추가

--- src/main/java/hello/board/service/CommentService.java
+++ src/main/java/hello/board/service/CommentService.java
@@ -1,8 +1,10 @@
 package hello.board.service;
 
+import hello.board.model.entity.Comment;
 import hello.board.model.response.BoardResponse;
 import hello.board.respository.BoardRepository;
 import hello.board.respository.BoardRepositoryCustom;
+import hello.board.respository.CommentRepositoryCustom;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +16,7 @@ public class CommentService {
 
     private final BoardRepository boardRepository;
     private final BoardRepositoryCustom boardRepositoryCustom;
+    private final CommentRepositoryCustom commentRepositoryCustom;
 
     public BoardResponse postComment(Long boardId, String commentBody) {
         return boardRepositoryCustom.find(boardId)
@@ -24,5 +27,11 @@ public class CommentService {
                 .map(boardRepository::save) // 게시글 + 댓글 저장
                 .map(BoardResponse::from) // 응답 객체로 변환
                 .orElseThrow(() -> new RuntimeException("존재하지 않는 게시글입니다."));
+    }
+
+    public void editComment(Long boardNo, Long commentNo, String commentBody) {
+        Comment comment = commentRepositoryCustom.find(commentNo, boardNo);
+        if (comment == null) throw new RuntimeException("존재하지 않는 게시글입니다.");
+        comment.setBody(commentBody);
     }
 }
