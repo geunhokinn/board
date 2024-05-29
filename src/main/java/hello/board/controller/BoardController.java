5b969f8d9bbb181e7ca1e840837e5bc591375045
geunhokinn <gghh1220@gmail.com>
2024-05-29 14:27:14 +0900

    feat: 게시판 리스트 조회 컨트롤러 추가

--- src/main/java/hello/board/controller/BoardController.java
+++ src/main/java/hello/board/controller/BoardController.java
@@ -6,13 +6,25 @@ import hello.board.model.request.BoardWriteRequest;
 import hello.board.model.response.BoardResponse;
 import hello.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class BoardController {
 
     private final BoardService boardService;
+
+    // 게시글 조회(게시판 리스트 조회)
+    @GetMapping("boardList") // 페이징 기법 사용
+    public List<BoardResponse> searchBoardList(
+            @RequestParam int page, // 페이지 번호
+            @RequestParam int pageSize, // 페이지의 데이터 개수
+            @RequestParam Sort.Direction direction) { // 정렬 기준
+        return  boardService.searchBoardList(page, pageSize, direction);
+    }
 
     // 게시글 조회(단일 게시글 조회)
     @GetMapping("board")
