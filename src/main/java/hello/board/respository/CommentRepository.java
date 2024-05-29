5c302f72273c7f4de4f4c36ff18db011053e73da
geunhokinn <gghh1220@gmail.com>
2024-05-29 23:44:58 +0900

    feat: 댓글 삭제 레파지토리 추가

---
+++ src/main/java/hello/board/respository/CommentRepository.java
@@ -0,0 +1,9 @@
+package hello.board.respository;
+
+import hello.board.model.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
