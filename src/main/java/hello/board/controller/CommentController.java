431994f2ab489262a877843f154d43a4f06a6dd8
geunhokinn <gghh1220@gmail.com>
2024-05-29 23:24:18 +0900

    feat: 댓글 수정 컨트롤러 추가

--- src/main/java/hello/board/controller/CommentController.java
+++ src/main/java/hello/board/controller/CommentController.java
@@ -1,10 +1,12 @@
 package hello.board.controller;
 
+import hello.board.model.request.CommentEditRequest;
 import hello.board.model.request.CommentPostRequest;
 import hello.board.model.response.BoardResponse;
 import hello.board.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,12 @@ public class CommentController {
     @PostMapping("comment")
     public BoardResponse post(@RequestBody CommentPostRequest commentPostRequest) {
         return commentService.postComment(commentPostRequest.getBoardNo(), commentPostRequest.getCommentBody());
+    }
+
+    @PutMapping("comment")
+    public String edit(@RequestBody CommentEditRequest commentEditRequest) {
+        commentService.editComment(commentEditRequest.getBoardNo(), commentEditRequest.getCommentNo(), commentEditRequest.getCommentBody());
+
+        return "OK";
     }
 }
