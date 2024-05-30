d7e461b489a7c7cc5894752ed67b689b4f49d371
geunhokinn <gghh1220@gmail.com>
2024-05-30 11:59:39 +0900

    feat: 댓글이 있는 게시글 삭제에서 추가 쿼리 해결 레파지토리 추가

--- src/main/java/hello/board/respository/CommentRepository.java
+++ src/main/java/hello/board/respository/CommentRepository.java
@@ -2,8 +2,17 @@ package hello.board.respository;
 
 import hello.board.model.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Modifying
+    @Query(value = "UPDATE comment SET comment_status='DELETE' where comment_no IN (:commentNos)", nativeQuery = true)
+    void deleteCommentList(@Param("commentNos") List<Long> commentNos);
 }
--- src/main/java/hello/board/service/BoardService.java
+++ src/main/java/hello/board/service/BoardService.java
@@ -2,8 +2,11 @@ package hello.board.service;
 
 import hello.board.model.entity.Board;
 import hello.board.model.entity.BoardStatus;
+import hello.board.model.entity.Comment;
 import hello.board.model.response.BoardResponse;
 import hello.board.respository.BoardRepository;
+import hello.board.respository.BoardRepositoryCustom;
+import hello.board.respository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -11,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +22,8 @@ import java.util.List;
 public class BoardService {
 
     private final BoardRepository boardRepository;
+    private final CommentRepository commentRepository;
+    private final BoardRepositoryCustom boardRepositoryCustom;
 
     public List<BoardResponse> searchBoardList(int page, int pageSize, Sort.Direction direction) {
         return boardRepository.findAll(PageRequest.of(page, pageSize, Sort.by(direction, "boardNo")))
@@ -49,8 +55,9 @@ public class BoardService {
     }
 
     public Long deleteBoard(Long boardNo) {
-        return boardRepository.findById(boardNo)
+        return boardRepositoryCustom.find(boardNo)
                 .map(board -> {
+                    commentRepository.deleteCommentList(board.getComments().stream().map(Comment::getCommentNo).collect(Collectors.toList()));
                     boardRepository.delete(board);
                     return board.getBoardNo(); // 게시물 번호
                 })
